5b58c52212aa9732f0f18491a0481e525b0b08a7
Eduard <timiseduard@yahoo.com>
2023-02-19 19:58:46 +0100

    signin working in postman

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: 1,
+            username: 'eduard',
+            email: 'eduard@yahoo.com',
+            password: 'admin',
+            entries: 0,
+            joined: new Date(),
+        },
+        {
+            id: 2,
+            username: 'Andrei',
+            email: 'Andrei@example.com',
+            password: 'admin2',
+            entries: 0,
+            joined: new Date(),
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.post('/signin', (req, res) => {
+    console.log(req.body.email, req.body.password);
+    console.log(database.users[0].email, database.users[0].password);
+    if (
+        req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password
+        ){
+        res.json('success');
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
 })
 
 app.listen(3000, () => {
