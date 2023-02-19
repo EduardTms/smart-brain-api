6002e1d010c47e1298734d78f30120c8076d5252
Eduard <timiseduard@yahoo.com>
2023-02-19 19:33:08 +0100

    plans

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 })
+
+// root route
+// => res = this is working
+
+// signin --> POST = success/fail
+
+// register --> POST = user
+
+// profile/:userId --> GET = user
+
+// image --> PUT --> user
