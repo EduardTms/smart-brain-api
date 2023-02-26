f464de88feee2093ef2030f72123a2cc40b2338f
EduardTms <timiseduard@yahoo.com>
2023-02-26 23:51:42 +0100

    signin can no longer check empty fields

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,7 @@
 const handleSignin = (req, res, db, bcrypt) => {
+  if (!email || !password) {
+    return res.status(400).json("Please enter all fields");
+  }
   db.select("email", "hash")
     .from("login")
     .where("email", "=", req.body.email)
