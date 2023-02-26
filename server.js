df0810a630a07581020d47758b62c6de37a87b96
EduardTms <timiseduard@yahoo.com>
2023-02-26 23:35:25 +0100

    moved signin to controllers folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const knex = require("knex");
 // Controllers
 const register = require("./controllers/register");
+const signin = require("./controllers/signin");
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,25 +24,7 @@ const db = knex({
 
 // signin --> POST = success/fail
 app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json("unable to find user"));
-      } else {
-        res.status(400).json("invalid credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("wrong credentials"));
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 // register --> POST = user
